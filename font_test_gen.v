d5ebacec7644e8ac0266f61f4fc35b24a7805fa2
GusRodGutTEC <gustavorodgut@gmail.com>
2016-08-27 14:04:26 -0600

    Add files via upload
    version correcta final

--- font_test_gen.v
+++ font_test_gen.v
@@ -5,79 +5,70 @@ module font_test_gen(
    input wire clk,
    input wire video_on,
    input wire [9:0] pixel_x, pixel_y,
-	input wire [2:0] color_rgb,
-	output reg [2:0] rgb_text
-//	output wire text_bit_on, //salida agregada para prueba
-//	output wire font_bit
+	input wire swt1, swt2, swt3,
+	output reg [2:0] rgb_text,
+	output wire font_bit
 
     );
 
    // signal declaration
-	//reg [6:0] char_addr0;
-   wire [10:0] rom_addr;
+	wire [10:0] rom_addr;
    reg [6:0] char_addr;
    wire [3:0] row_addr;
    wire [2:0] bit_addr;
    wire [7:0] font_word;
-	wire text_bit_on;
-	wire font_bit;
-	wire [2:0] px_color;
+	wire [2:0] rgb_graph;
 
    // body
    // instantiate font ROM
    font_rom font_unit
       (.clk(clk), .addr(rom_addr), .data(font_word));
-		
-	//instanciando deco colores
-color_case lcolor (
-    .color_rgb(color_rgb), 
-    .px_color(px_color)
-    );
-		
-		
+
    // font ROM interface
- //  assign char_addr = {pixel_y[5:4], pixel_x[7:3]};
    assign row_addr = pixel_y[3:0];
    assign rom_addr = {char_addr, row_addr};
    assign bit_addr = pixel_x[2:0];
    assign font_bit = font_word[~bit_addr];
-	
+
 		
    // region delimitada para el texto
-   assign text_bit_on = (pixel_y[9:5]==15) && //Me determina el lugar donde se proyectaran las iniciales
-                    (38<=pixel_x[9:3]) && (pixel_x[9:3]<=40);
-						  
+   assign text_bit_on = (pixel_y[9:4]==12);
 	always @*
+
       case(pixel_x[8:3])
-         6'h26: char_addr = 7'h4a; // J
+         6'h25: char_addr = 7'h4a; // J
+					
          6'h27: char_addr = 7'h4d; // M
-			6'h28: char_addr = 7'h47; // G
+					
+			6'h29: char_addr = 7'h47; // G
+					
          default: char_addr = 7'h00; //espacio en blanco
       endcase
-								
+
+	assign rgb_graph[2] = swt1;   // red
+	assign rgb_graph[1] = swt2;
+	assign rgb_graph[0] = swt3; 
+	
 
    // rgb multiplexing circuit
    always @*
-      if (~video_on)
-         rgb_text = 3'b000; // zona prohibida color negro
+      if (~video_on) //se cambia a rojo
+         rgb_text = 3'b000; // zona prohibida
       else
 			begin
-				if (text_bit_on) //hay que dibujar letra
+			if (~text_bit_on)
+				begin
+				rgb_text=3'b000;
+				end
+			else
+				if (font_bit) //hay que pintar pixel letra
 					begin
-					if (font_bit) //hay que pintar pixel letra
-						begin
-						rgb_text=px_color; //color escogido
-						end
-					else
-						begin
-						rgb_text=3'b011; //mismo color del fondo de pantalla
-						end
+					rgb_text = rgb_graph;
 					end
 				else
 					begin
-					rgb_text=3'b011; //color fondo de pantalla cyan
+					rgb_text=3'b000; //mismo color del fondo de pantalla
 					end
 			end
-				
 
 endmodule
